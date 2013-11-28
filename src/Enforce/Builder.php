1e00791f11630563d8a89a2e7c3d1585a21903d5
root <darryl.kuhn@gmail.com>
2013-11-28 09:59:29 -0800

    added firstOrFail() method and converted from tabs to spaces

--- src/Enforce/Builder.php
+++ src/Enforce/Builder.php
@@ -4,101 +4,114 @@ use \Illuminate\Database\Eloquent;
 
 class Builder extends Eloquent\Builder
 {
-	protected $enforceOnRead = null;
+    protected $enforceOnRead = null;
 
-	/**
-	 * Execute the query and get the first result.
-	 *
-	 * @param  array  $columns
-	 * @return \Illuminate\Database\Eloquent\Model|static|null
-	 */
-	public function first($columns = array('*'), $enforceOnRead=null)
-	{
-		$this->enforceOnRead = $enforceOnRead === null ? \Config::get('enforce.byDefault') : $enforceOnRead;
-		return $this->take(1)->get($columns)->first();
-	}
+    /**
+     * Execute the query and get the first result.
+     *
+     * @param  array  $columns
+     * @return \Illuminate\Database\Eloquent\Model|static|null
+     */
+    public function first($columns = array('*'), $enforceOnRead=null)
+    {
+        $this->enforceOnRead = $enforceOnRead === null ? \Config::get('enforce.byDefault') : $enforceOnRead;
+        return $this->take(1)->get($columns)->first();
+    }
 
-	/**
-	 * Find a model by its primary key.
-	 *
-	 * @param  mixed  $id
-	 * @param  array  $columns
-	 * @return \Illuminate\Database\Eloquent\Model|static|null
-	 */
-	public function find($id, $columns = array('*'), $enforceOnRead=null)
-	{
-		$this->enforceOnRead = $enforceOnRead === null ? \Config::get('enforce.byDefault') : $enforceOnRead;
+    /**
+     * Execute the query and get the first result or throw an exception.
+     *
+     * @param  array  $columns
+     * @return \Illuminate\Database\Eloquent\Model|static
+     */
+    public function firstOrFail($columns = array('*'), $enforceOnRead=null)
+    {
+        if ( ! is_null($model = $this->first($columns, $enforceOnRead))) return $model;
 
-		if (is_array($id))
-		{
-		    $results = $this->findMany($id, $columns);
-		}
-		else 
-		{
-			$this->query->where($this->model->getKeyName(), '=', $id);
+        throw new ModelNotFoundException;
+    }
 
-			$results = $this->first($columns, $enforceOnRead);
-		}
+    /**
+     * Find a model by its primary key.
+     *
+     * @param  mixed  $id
+     * @param  array  $columns
+     * @return \Illuminate\Database\Eloquent\Model|static|null
+     */
+    public function find($id, $columns = array('*'), $enforceOnRead=null)
+    {
+        $this->enforceOnRead = $enforceOnRead === null ? \Config::get('enforce.byDefault') : $enforceOnRead;
 
-		return $results;
-	}
+        if (is_array($id))
+        {
+            $results = $this->findMany($id, $columns);
+        }
+        else 
+        {
+            $this->query->where($this->model->getKeyName(), '=', $id);
 
-	/**
-	 * Find a model by its primary key or throw an exception.
-	 *
-	 * @param  mixed  $id
-	 * @param  array  $columns
-	 * @return \Illuminate\Database\Eloquent\Model|static
-	 */
-	public function findOrFail($id, $columns = array('*'), $enforceOnRead=null)
-	{
-		$this->enforceOnRead = $enforceOnRead === null ? \Config::get('enforce.byDefault') : $enforceOnRead;
+            $results = $this->first($columns, $enforceOnRead);
+        }
 
-		if ( ! is_null($model = $this->find($id, $columns, $enforceOnRead))) return $model;
+        return $results;
+    }
 
-		throw new Eloquent\ModelNotFoundException;
-	}
+    /**
+     * Find a model by its primary key or throw an exception.
+     *
+     * @param  mixed  $id
+     * @param  array  $columns
+     * @return \Illuminate\Database\Eloquent\Model|static
+     */
+    public function findOrFail($id, $columns = array('*'), $enforceOnRead=null)
+    {
+        $this->enforceOnRead = $enforceOnRead === null ? \Config::get('enforce.byDefault') : $enforceOnRead;
 
-	/**
-	 * Get the hydrated models without eager loading.
-	 *
-	 * @param  array  $columns
-	 * @return array|static[]
-	 */
-	public function getModels($columns = array('*'))
-	{
-		// First, we will simply get the raw results from the query builders which we
-		// can use to populate an array with Eloquent models. We will pass columns
-		// that should be selected as well, which are typically just everything.
-		$results = $this->query->get($columns);
+        if ( ! is_null($model = $this->find($id, $columns, $enforceOnRead))) return $model;
 
-		$connection = $this->model->getConnectionName();
+        throw new Eloquent\ModelNotFoundException;
+    }
 
-		$models = array();
+    /**
+     * Get the hydrated models without eager loading.
+     *
+     * @param  array  $columns
+     * @return array|static[]
+     */
+    public function getModels($columns = array('*'))
+    {
+        // First, we will simply get the raw results from the query builders which we
+        // can use to populate an array with Eloquent models. We will pass columns
+        // that should be selected as well, which are typically just everything.
+        $results = $this->query->get($columns);
 
-		// Once we have the results, we can spin through them and instantiate a fresh
-		// model instance for each records we retrieved from the database. We will
-		// also set the proper connection name for the model after we create it.
-		foreach ($results as $result)
-		{
-			$model = $this->model->newFromBuilder($result);
-			$class = get_class($model);
+        $connection = $this->model->getConnectionName();
 
-			if ( $this->enforceOnRead )
-			{
-				if ( $model = $class::enforceOnRead($model) )
-				{
-					$model->setConnection($connection);
-					$models[] = $model;
-				}
-			}
-			else 
-			{
-				$model->setConnection($connection);
-				$models[] = $model;
-			}
-		}
+        $models = array();
 
-		return $models;
-	}
+        // Once we have the results, we can spin through them and instantiate a fresh
+        // model instance for each records we retrieved from the database. We will
+        // also set the proper connection name for the model after we create it.
+        foreach ($results as $result)
+        {
+            $model = $this->model->newFromBuilder($result);
+            $class = get_class($model);
+
+            if ( $this->enforceOnRead )
+            {
+                if ( $model = $class::enforceOnRead($model) )
+                {
+                    $model->setConnection($connection);
+                    $models[] = $model;
+                }
+            }
+            else 
+            {
+                $model->setConnection($connection);
+                $models[] = $model;
+            }
+        }
+
+        return $models;
+    }
 }
